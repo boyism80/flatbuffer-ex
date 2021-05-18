b28309ea12d595492d83c0f79377ad7082d6ac91
boyism80 <boyism80@gmail.com>
2021-05-18 21:20:18 +0900

    응애

--- modules/generator.py
+++ modules/generator.py
@@ -1,6 +1,7 @@
 import jinja2
 import os
 import copy
+import re
 import modules.extractor as extractor
 
 def loadTemplateFiles(root):
@@ -15,7 +16,9 @@ def __nameset(name):
     return {
         'base': name,
         'upper': name[0].upper() + name[1:],
-        'lower': name[0].lower() + name[1:]
+        'lower': name[0].lower() + name[1:],
+        'upper_snake': re.sub('(?!^)([A-Z]+)', r'_\1',name).upper(),
+        'lower_snake': re.sub('(?!^)([A-Z]+)', r'_\1',name).lower()
     }
 
 
